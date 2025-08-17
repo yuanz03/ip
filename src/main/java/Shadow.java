41926c1cd3a3277c7e06a6068e12f35dbf40a06c
yuanz03 <chinyuantian03@gmail.com>
2025-08-17 17:33:59 +0800

    Rename Chatbot and update greeting message

---
+++ src/main/java/Shadow.java
@@ -0,0 +1,9 @@
+public class Shadow {
+    public static void main(String[] args) {
+        System.out.println("""
+                Hi, I'm Shadow, your personal assistant!
+                What can I help you with today?
+                
+                Goodbye! I'll be here if you need anything else!""");
+    }
+}
