9642ae5f7f285bc9892726d6094c552425d9c9c3
yuanz03 <chinyuantian03@gmail.com>
2025-08-17 21:41:57 +0800

    Implement echo functionality for user input

--- src/main/java/Shadow.java
+++ src/main/java/Shadow.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Shadow {
     public static void main(String[] args) {
-        System.out.println("""
-                Hi, I'm Shadow, your personal assistant!
-                What can I help you with today?
-                
-                Goodbye! I'll be here if you need anything else!""");
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Hi, I'm Shadow, your personal assistant!\nWhat can I help you with today?\n");
+
+        String userInput = inputScanner.next();
+        while (!(userInput.equalsIgnoreCase("bye"))) {
+            System.out.println("\n" + userInput + "\n");
+            userInput = inputScanner.next();
+        }
+        System.out.println("\nGoodbye! I'll be here if you need anything else!");
     }
 }
