1d5e53e8bc47d6274e96243d10ab885af29d89ce
yuanz03 <chinyuantian03@gmail.com>
2025-09-14 15:49:54 +0800

    Add colour and text bubble support to Shadow GUI

--- src/main/java/shadowbuddy/gui/DialogBox.java
+++ src/main/java/shadowbuddy/gui/DialogBox.java
@@ -52,6 +52,7 @@ public class DialogBox extends HBox {
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
+        dialog.getStyleClass().add("reply-label");
     }
 
     /**
